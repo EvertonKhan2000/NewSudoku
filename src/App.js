825a9d4915a3537f71edf912e6248181896508ef
L-E-E-H-E-E <51725455+L-E-E-H-E-E@users.noreply.github.com>
2020-06-16 12:44:42 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import './App.css';
 
 function App() {
   return (
-
+<div>
     <p> Hello there evie </p>
     <button
-      onPress={()=> {console.log("aagh")}}
+
+    
+
+      onPress={() => { console.log("aagh") }}
       title="Solve"
       color="#841584"
       accessibilityLabel="We out here solving"
-      >
-      
+>
     </button>
+
+    </div>
 
   );
 }
