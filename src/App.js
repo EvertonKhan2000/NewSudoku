004e767f7092605a07013bbf56ab22d8d2a7553e
L-E-E-H-E-E <51725455+L-E-E-H-E-E@users.noreply.github.com>
2020-06-16 12:31:05 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ function App() {
   return (
    
     <p> Hello there evie </p>
-    
+
 
   );
 }
+
+<Button
+  onPress={}
+  title="Solve"
+  color="#841584"
+  accessibilityLabel="We out here solving"
+/>
 
 export default App;
