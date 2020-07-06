70f3496903ae2df44e18180cecabaefc966105ea
L-E-E-H-E-E <51725455+L-E-E-H-E-E@users.noreply.github.com>
2020-07-06 12:51:26 +1000

    Update Testing.js

--- src/Testing.js
+++ src/Testing.js
@@ -138,6 +138,7 @@ function whichBox(whereRowY, whereColX) {
 // takes a no. (whichNo), checks if it is already in the row, col, or box specified (whereNo)
 export function numberCheck(Matrix, whichNo, whereRowY, whereColX) {
     let check = false;
+    if (whichNo < 1 || whichNo > size * size ) console.log('whoopsy daisy bubba');
     console.log(whichNo)
 
     if (rowCheck(Matrix,whereRowY, whichNo)) {
